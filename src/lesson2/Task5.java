67d53507302623f8d39d3bf2dd3b0bfe77230335
Vadim Balakirev <it-balakirev@mail.ru>
2020-01-20 16:46:32 +0300

    Lesson2 ► Task5: logic min added.

--- src/lesson2/Task5.java
+++ src/lesson2/Task5.java
@@ -3,16 +3,16 @@ package lesson2;
 public class Task5 {
 
     public static void main(String[] args) {
-        int[] array = {8, 6, 2, 10, 4, 15, 1};
+        int[] array = {8, 6, 2, 10, 4, 15, 7};
         int maxarr = 0;
-        int minarr = 0;
+        int minarr = array[0];
         for (int i = 0; i < array.length; i++) {
             if (array[i] > maxarr) {
                 maxarr = array[i];
             }
         }
         for (int i = 0; i < array.length; i++) {
-            if (array[i] < maxarr) {
+            if (minarr > array[i]) {
                 minarr = array[i];
             }
         }

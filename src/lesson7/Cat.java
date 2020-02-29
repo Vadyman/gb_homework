61f36c8055ab6ce3c83eaa0a6ba39a4880ceebd7
Vadim Balakirev <it-balakirev@mail.ru>
2020-02-29 20:26:54 +0300

    Lesson7 ► Added class Cat, Plate, Test7, HomeWork7.

---
+++ src/lesson7/Cat.java
@@ -0,0 +1,47 @@
+package lesson7;
+
+public class Cat {
+
+    private final String name;
+    private int appetite;
+    private boolean satiety;
+
+    public Cat(String name, int appetite) {
+        this.name = name;
+        this.appetite = appetite;
+        this.satiety = satiety;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAppetite() {
+        return appetite;
+    }
+
+    public void setAppetite(int appetite) {
+        this.appetite = appetite;
+    }
+
+    public boolean eat(Plate plate) {
+        if (plate.decreaseFood(appetite) && !satiety ) {
+            satiety = true;
+            return true;
+        }
+        return false;
+    }
+
+    public void info() {
+        System.out.println(this);
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "name='" + name + '\'' +
+                ", appetite=" + appetite +
+                ", satiety=" + satiety +
+                '}';
+    }
+}
